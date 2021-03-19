9d550f128322ab14f7660ab5880f3c4811cc3b87
__Cielll__ <u3576776@connect.hku.hk>
2021-03-20 06:59:00 +0800

    Fix errors in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,32 @@
 #include <bits/stdc++.h>
-#include <map.h>
-using namespace std;
+#include "map.h"
 
-void printMap(map map){
-    for (int i=0; i<20; i++){
-        for (int j=0; j<20; j++)
-            printf("%d", map.map_query(i, j));
-        printf("\n");
-    }
-}
+mp::map *board;
 
 // Purpose: game interface
 int main(){
-	map map;
-    printMap(map);
+    board = new mp::map();
+    for (int i=0; i<20; i++){
+        for (int j=0; j<20; j++)
+            printf("%d", board->map_query(i, j));
+        printf("\n");
+    }
 
     int x, y, score;
     int score_total = 0;
     
-    while (!map.is_end()){ // while some blocks can be eliminated
+    while (!board->is_end()){ // while some blocks can be eliminated
         scanf("%d%d", &x, &y); // read x,y coords
-        score = map.operation(x, y); // eliminate blocks based on the given x, y
+        score = board->operation(x, y); // eliminate blocks based on the given x, y
         score_total += score; // compute total score
-        printMap(map);
+        for (int i=0; i<20; i++){
+            for (int j=0; j<20; j++)
+                printf("%d", board->map_query(i, j));
+            printf("\n");
+        }
     }
-    printf("%d", reward_total);
+    printf("%d", score_total);
 
     return 0;
 }
+
