2281b8f20f2430eb3f81a448b36d55c16b1206ed
__Cielll__ <u3576776@connect.hku.hk>
2021-03-19 22:26:20 +0800

    Add map display feature

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,19 @@
 #include <map.h>
 using namespace std;
 
+void printMap(map map){
+    for (int i=0; i<20; i++){
+        for (int j=0; j<20; j++)
+            printf("%d", map.map_query(i, j));
+        printf("\n");
+    }
+}
+
 // Purpose: game interface
 int main(){
-	Map map;
+	map map;
+    printMap(map);
+
     int x, y, score;
     int score_total = 0;
     
@@ -12,6 +22,7 @@ int main(){
         scanf("%d%d", &x, &y); // read x,y coords
         score = map.operation(x, y); // eliminate blocks based on the given x, y
         score_total += score; // compute total score
+        printMap(map);
     }
     printf("%d", reward_total);
 
