432f61c11d670e285c4d62d3157c4a244dea5aad
cogito233 <cogito@connect.hku.hk>
2021-03-19 21:36:08 +0800

    prototype of map

--- src/map.cpp
+++ src/map.cpp
@@ -1,22 +1,44 @@
 #include <map.h>
-class map{
-	private:
-		int map[30][30];
-	public:
-		map(int difficulty_coefficient) {
-			
-		}
-		int map_query(int x,int y) {
-			
-		}
-		bool is_end() {
-			
-		}
-		int operation(int x, int y) {
-			
-		}
-		int reward_score() {
-			
-		}
-}
 
+namespace map{
+	
+	
+	map::map(int difficulty_coefficient) {
+		srand(time(0));
+		for (int i = 1; i <= 20; i++)
+			for (int j = 1; j <= 20; j++)
+				this->map[i][j] = random() % 3 + 1;
+	}
+	
+	int map::map_query(int x,int y) {
+		return this->map[x][y];
+	}
+	
+	bool map::is_end() {
+		bool flag = 1;
+		for (int i = 1; i <= 20; i++)
+			for (int j = 1; j <= 20; j++)
+				flag = flag && !(this->map[i][j] == this->map[i][j-1] || this->map[i][j] == this->map[i-1][j]);
+		return flag;
+	}
+	
+	int map::operation(int x, int y) {
+		if (this->map[x][y] == 0) return -1;
+		if (!(this->map[i][j] == this->map[i][j-1] || this->map[i][j] == this->map[i-1][j] 
+			|| this->map[i][j] == this->map[i+1][j] || this->map[i][j] == this->map[i][j+1])) return -1;
+		int sum = 0;
+		this->dfs(i, j, f[i][j], sum);
+		
+		for (int i = 1; i <= 20; i++)
+			
+		return sum * sum * 5;
+	}
+	
+	int map::reward_score() {
+		int sum=1000;
+		for (int i = 1; i <= 20; i++)
+			for (int j = 1; j <= 20; j++)
+				if (a[i][j]) sum-=100;
+		return sum<0 ? 0 : sum;
+	}
+}
--- src/map.h
+++ src/map.h
@@ -1,12 +1,20 @@
+#ifndef map_h
+#define map_h
+
+#include <time.h>
+
 namespace map{
 	class map{
 		private:
 			int map[30][30];
+			int evaluate_map();
+			int dfs(int x,int y,int colour,int &sum);
 		public:
-			map(int difficulty_coefficient);//生产地图(构造函数)
-			int map_query(int x,int y);//返回某个点的颜色？
+			map(int difficulty_coefficient);//生成地图(构造函数)
+			int map_query(int x, int y);//返回某个点的颜色？
 			bool is_end();//返回0代表地图已经不可消除了
-			int operation(int x,int y);//以x,y为中心消除，返回该操作的得分
+			int operation(int x, int y);//以x,y为中心消除，返回该操作的得分
 			int reward_score();//返回当前游戏的奖励分
 	}
 }
+#endif
