ecac371b79e95a8c80c5da535e0c7c9ec6d823c5
wwwCielwww <u3576776@connect.hku.hk>
2021-04-01 13:28:22 +0800

    Update Makefile

--- get.sh
+++ get.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-if [ ! -d ncurses ]
+if [ ! -d ../ncurses ]
 then 
-    echo "Start building dependencies..."
-    echo "Start downloading ncurses"
     git clone https://github.com/mirror/ncurses.git
-    echo "Downloaded successfully"
 
     cd ncurses
-    echo "Configuring..."
     mkdir ~/local
     ./configure --prefix ~/local
 
-    echo "Installing..."
     make -j
     make -j install
-    
 fi
 
 echo "You're all set"
