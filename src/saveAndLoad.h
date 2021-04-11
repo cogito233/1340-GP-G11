36a1eee731f62a1b467dfe44aeb7f4c5c548cec7
cogito233 <cogito@connect.hku.hk>
2021-04-11 06:18:10 -0700

    clang format

--- src/saveAndLoad.h
+++ src/saveAndLoad.h
@@ -2,42 +2,44 @@
 #define sl_h
 
 #include "map.h"
-#include <string>
 #include <fstream>
-#include <time.h>
 #include <ncurses.h>
+#include <string>
+#include <time.h>
 
-namespace sl{
-    struct ranklist{
-        std::string name;
-        int score;
-    };
+namespace sl {
+struct ranklist {
+  std::string name;
+  int score;
+};
 
-    class rank_list {
-        private:
-            ranklist rk[10];
-            int ranker_num;
-            std::string filename;
-        public:
-            rank_list(std::string filename);
-            void sort();
-            void add(std::string name, int score);
-            int save();
-            void print_rank();
-    };
-    
-    class current_map {
-        private:
-            mp::map *board;
-            int *round;
-        public:
-            current_map(mp::map *board, int &round);
-            //void link_map(mp::map *board);
-            //void set_round(int round);
-            int get_round();
-            int save(std::string name);
-            int load(std::string filename);
-    };
-}
+class rank_list {
+private:
+  ranklist rk[10];
+  int ranker_num;
+  std::string filename;
+
+public:
+  rank_list(std::string filename);
+  void sort();
+  void add(std::string name, int score);
+  int save();
+  void print_rank();
+};
+
+class current_map {
+private:
+  mp::map *board;
+  int *round;
+
+public:
+  current_map(mp::map *board, int &round);
+  // void link_map(mp::map *board);
+  // void set_round(int round);
+  int get_round();
+  int save(std::string name);
+  int load(std::string filename);
+};
+} // namespace sl
 
 #endif
