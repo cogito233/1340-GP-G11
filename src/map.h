9c3e6313e7b1e4d26f4f6d7e1afe618d6151a004
wwwCielwww <u3576776@connect.hku.hk>
2021-04-11 20:47:32 +0800

    Formatted code

--- src/map.h
+++ src/map.h
@@ -3,22 +3,23 @@
 
 #include <time.h>
 
-namespace mp{
-	class map{
-		private:
-			int f[30][30],score;
-			void copy_column(int x,int y);
-			void normalization();
-			void dfs(int x,int y,int colour,int &sum);
-		public:
-			map();//生成地图(构造函数)
-			int now_score();
-			void score_set(int val);
-			int map_query(int x, int y);//返回某个点的颜色？
-			void map_set(int x, int y, int val);
-			bool is_end();//返回0代表地图已经不可消除了
-			int operation(int x, int y);//以x,y为中心消除，返回该操作的得分
-			void display();//显示地图
-	};
-}
+namespace mp {
+class map {
+private:
+  int f[30][30], score;
+  void copy_column(int x, int y);
+  void normalization();
+  void dfs(int x, int y, int colour, int &sum);
+
+public:
+  map(); //生成地图(构造函数)
+  int now_score();
+  void score_set(int val);
+  int map_query(int x, int y); //返回某个点的颜色？
+  void map_set(int x, int y, int val);
+  bool is_end();               //返回0代表地图已经不可消除了
+  int operation(int x, int y); //以x,y为中心消除，返回该操作的得分
+  void display();              //显示地图
+};
+} // namespace mp
 #endif
