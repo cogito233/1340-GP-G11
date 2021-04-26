46288bf7b4560ace4e41b041bd0bee9759c949a2
cogito233 <cogito@connect.hku.hk>
2021-04-25 22:42:33 -0700

    Update map.cpp

--- src/map.cpp
+++ src/map.cpp
@@ -10,6 +10,15 @@
 #include <ncurses.h>
 
 namespace mp {
+
+// 1. Count the size of the current link block (set sum as counter)
+// 2. eliminate the link block (f[x][y]->0)
+//
+// The initial input (x, y, colour) should be the coordinates 
+// and colours of the points being manipulated in the game
+//
+// Important: This function will only eliminate the link block, and
+// if you need to let the block fall, you'll use the normalization function
 void map::dfs(int x, int y, int colour, int &sum) {
   this->f[x][y] = 0;
   sum += 1;
@@ -30,6 +39,7 @@ void map::copy_column(int x, int y) {
     this->f[i][x] = this->f[i][y];
 }
 
+// Make the block go down and fall to the left
 void map::normalization() {
   for (int j = 1; j <= 20; j++) {
     int k = 21;
@@ -47,6 +57,7 @@ void map::normalization() {
     this->copy_column(k, 0);
 }
 
+// The map will be randomly generated
 map::map() {
   this->score = 0;
   srand(time(0));
@@ -55,12 +66,11 @@ map::map() {
       this->f[i][j] = rand() % 3 + 1;
 }
 
+//The following four functions are used to modify and query
+//class variables, which are protected because they are private
 int map::now_score() { return this->score; }
-
 void map::score_set(int val) { this->score = val; }
-
 int map::map_query(int x, int y) { return this->f[x][y]; }
-
 void map::map_set(int x, int y, int val) { this->f[x][y] = val; }
 
 bool map::is_end() {
@@ -73,6 +83,10 @@ bool map::is_end() {
   return flag;
 }
 
+// Method of operating on the (x, y) point of the map, inculding:
+//   1. Check if it is a correction link block thatcan be eliminated;
+//   2. Eliminate, maintain the map;
+//   3. Calculate the score for this operation.
 int map::operation(int x, int y) {
   if (this->f[x][y] == 0)
     return -1;
@@ -88,6 +102,7 @@ int map::operation(int x, int y) {
   return sum * sum * 5;
 }
 
+// Reserved for initial debugging
 void map::display() {
   for (int i = 1; i <= 20; i++) {
     for (int j = 1; j <= 20; j++)
